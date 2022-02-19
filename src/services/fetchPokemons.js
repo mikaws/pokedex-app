8eb90f1e0d547c7ec6d3d9bfd812b78715819f14
mikaws <michaelhenriqued@gmail.com>
2022-02-18 23:13:19 -0300

    refactor: move fetch api to services

---
+++ src/services/fetchPokemons.js
@@ -0,0 +1,16 @@
+const fetchPokemons = async (position) => {
+  let data =[]
+  let newPosition = 0;
+  if (position > 8) { 
+    newPosition = position - 8
+  }
+  const url = 'https://pokeapi.co/api/v2/pokemon/'
+  for(let i=1; position < 8 ? i<=8+position : i<=16; i++){
+    let res = await fetch(url +( position < 8 ? i : i + newPosition))
+    data.push(await res.json())
+  }
+
+  return data
+}
+
+export default fetchPokemons
