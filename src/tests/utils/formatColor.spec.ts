b19a7d3c7f3b367e1a80f88552a542abc7d7862f
mikaws <michaelhenriqued@gmail.com>
2022-04-20 19:50:50 -0300

    test: ensure formatColor util returns the correct color

---
+++ src/tests/utils/formatColor.spec.ts
@@ -0,0 +1,29 @@
+import { formatColor } from '../../utils/'
+
+const pokemonTypes = [
+  'normal', 'fire', 'fighting',
+  'water', 'flying', 'grass',
+  'poison', 'electric', 'ground',
+  'psychic', 'rock', 'ice',
+  'bug', 'dragon', 'ghost',
+  'dark', 'steel', 'fairy',
+  'unknown'
+]
+
+const pokemonTypeColors = [
+  '#A8A878', '#F08030', '#C03028',
+  '#6890F0', '#A890F0', '#78C850',
+  '#A040A0', '#F8D030', '#E0C068',
+  '#F85888', '#B8A038', '#98D8D8',
+  '#A8B820', '#7038F8', '#705898',
+  '#705848', '#B8B8D0', '#EE99AC',
+  '#A8A878'
+]
+
+describe('format color util', () => {
+  pokemonTypes.forEach((type, i) => {
+    it('should to return the correct color', () => {
+      expect(formatColor(type)).toBe(pokemonTypeColors[i])
+    })
+  })
+})
