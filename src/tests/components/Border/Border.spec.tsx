ad9fedcebfeb85052dd56a963f75aa313c5b3d9a
mikaws <michaelhenriqued@gmail.com>
2022-04-20 19:57:53 -0300

    test: ensure that Border component renders correctly

---
+++ src/tests/components/Border/Border.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import PokedexBorder from '../../../components/Border/Border'
+
+describe('PokedexBorder component', () => {
+  it('should render correctly', () => {
+    const { asFragment } = render(<PokedexBorder />)
+    expect(asFragment().childElementCount).toBe(2)
+  })
+})
